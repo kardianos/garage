5baab02c5a8929f4a87860ffc980e1f3c37d8335
Daniel Theophanes <kardianos@gmail.com>
2015-10-31 22:13:54 -0700

    gclient: add two lines of text.

--- gclient/main.go
+++ gclient/main.go
@@ -37,19 +37,12 @@ import (
 	"golang.org/x/mobile/event/paint"
 	"golang.org/x/mobile/event/size"
 	"golang.org/x/mobile/event/touch"
-	"golang.org/x/mobile/exp/app/debug"
 	"golang.org/x/mobile/exp/gl/glutil"
 	"golang.org/x/mobile/gl"
 )
 
 var (
-	images   *glutil.Images
-	fps      *debug.FPS
-	program  gl.Program
-	position gl.Attrib
-	offset   gl.Uniform
-	color    gl.Uniform
-	buf      gl.Buffer
+	images *glutil.Images
 
 	green  float32
 	touchX float32
@@ -69,11 +62,9 @@ func main() {
 				case lifecycle.CrossOn:
 					glctx, _ = e.DrawContext.(gl.Context)
 					vs.start(glctx)
-					// onStart(glctx)
 					a.Send(paint.Event{})
 				case lifecycle.CrossOff:
 					vs.end(glctx)
-					// onStop(glctx)
 					glctx = nil
 				}
 			case size.Event:
@@ -89,7 +80,6 @@ func main() {
 				}
 
 				vs.draw(glctx, sz)
-				// onPaint(glctx, sz)
 				a.Publish()
 				// Drive the animation by preparing to paint the next frame
 				// after this one is shown.
@@ -109,7 +99,6 @@ type viewState struct {
 	tc chan touch.Event
 
 	sq *Square
-	// Describe sequential regions.
 }
 
 func newViewState() *viewState {
@@ -157,88 +146,5 @@ func (vs *viewState) draw(glctx gl.Context, sz size.Event) {
 
 	glctx.Clear(gl.COLOR_BUFFER_BIT)
 
-	vs.sq.Draw(glctx, sz)
+	vs.sq.Draw(glctx, sz, "Garage door opener", "Tap to toggle garage door")
 }
-
-/*
-func onStart(glctx gl.Context) {
-	var err error
-	program, err = glutil.CreateProgram(glctx, vertexShader, fragmentShader)
-	if err != nil {
-		log.Printf("error creating GL program: %v", err)
-		return
-	}
-
-	buf = glctx.CreateBuffer()
-	glctx.BindBuffer(gl.ARRAY_BUFFER, buf)
-	glctx.BufferData(gl.ARRAY_BUFFER, triangleData, gl.STATIC_DRAW)
-
-	position = glctx.GetAttribLocation(program, "position")
-	color = glctx.GetUniformLocation(program, "color")
-	offset = glctx.GetUniformLocation(program, "offset")
-
-	images = glutil.NewImages(glctx)
-	fps = debug.NewFPS(images)
-}
-
-func onStop(glctx gl.Context) {
-	glctx.DeleteProgram(program)
-	glctx.DeleteBuffer(buf)
-	fps.Release()
-	images.Release()
-}
-
-func onPaint(glctx gl.Context, sz size.Event) {
-	glctx.ClearColor(1, 0, 0, 1)
-	glctx.Clear(gl.COLOR_BUFFER_BIT)
-
-	glctx.UseProgram(program)
-
-	green += 0.01
-	if green > 1 {
-		green = 0
-	}
-	glctx.Uniform4f(color, 0, green, 0, 1)
-
-	glctx.Uniform2f(offset, touchX/float32(sz.WidthPx), touchY/float32(sz.HeightPx))
-
-	glctx.BindBuffer(gl.ARRAY_BUFFER, buf)
-	glctx.EnableVertexAttribArray(position)
-	glctx.VertexAttribPointer(position, coordsPerVertex, gl.FLOAT, false, 0, 0)
-	glctx.DrawArrays(gl.TRIANGLES, 0, vertexCount)
-	glctx.DisableVertexAttribArray(position)
-
-	fps.Draw(sz)
-}
-*/
-
-/*
-var triangleData = f32.Bytes(binary.LittleEndian,
-	0.0, 0.4, 0.0, // top left
-	0.0, 0.0, 0.0, // bottom left
-	0.4, 0.0, 0.0, // bottom right
-)
-
-const (
-	coordsPerVertex = 3
-	vertexCount     = 3
-)
-
-const vertexShader = `#version 100
-uniform vec2 offset;
-
-attribute vec4 position;
-void main() {
-	// offset comes in with x/y values between 0 and 1.
-	// position bounds are -1 to 1.
-	vec4 offset4 = vec4(2.0*offset.x-1.0, 1.0-2.0*offset.y, 0, 0);
-	gl_Position = position + offset4;
-}`
-
-const fragmentShader = `#version 100
-precision mediump float;
-uniform vec4 color;
-void main() {
-	gl_FragColor = color;
-}`
-*/
--- gclient/square.go
+++ gclient/square.go
@@ -5,13 +5,9 @@
 package main
 
 import (
-	"encoding/binary"
-	"fmt"
 	"image"
-	"time"
 
 	"golang.org/x/mobile/event/size"
-	"golang.org/x/mobile/exp/f32"
 	"golang.org/x/mobile/exp/font"
 	"golang.org/x/mobile/exp/gl/glutil"
 	"golang.org/x/mobile/geom"
@@ -25,17 +21,7 @@ import (
 )
 
 type Square struct {
-	program  gl.Program
-	position gl.Attrib
-	offset   gl.Uniform
-	color    gl.Uniform
-	buf      gl.Buffer
-
-	green float32
-	down  bool
-	x, y  float32
-	last  time.Time
-	inc   float32
+	percent float32
 
 	images *glutil.Images
 	img1   *glutil.Image
@@ -44,49 +30,27 @@ type Square struct {
 }
 
 func NewSquare(glctx gl.Context, inc, x, y float32) (*Square, error) {
-	program, err := glutil.CreateProgram(glctx, vertexShader, fragmentShader)
-	if err != nil {
-		return nil, fmt.Errorf("error creating GL program: %v", err)
-	}
-
-	var (
-		sqX          float32 = x
-		sqY          float32 = y
-		triangleData         = f32.Bytes(binary.LittleEndian,
-			0.0, sqY, 0.0, // top left
-			0.0, 0.0, 0.0, // bottom left
-			sqX, 0.0, 0.0, // bottom right
-
-			sqX, sqY, 0.0, // top right
-			sqX, 0.0, 0.0, // bottom right
-			0.0, sqY, 0.0, // bottom left
-		)
-	)
-
 	images := glutil.NewImages(glctx)
+	img1 := images.NewImage(800, 200)
 
 	ftctx := freetype.NewContext()
 	ftFont, err := freetype.ParseFont(font.Default())
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	ftctx.SetFont(ftFont)
 
-	sq := &Square{
-		images:  images,
-		img1:    images.NewImage(400, 200),
-		ftctx:   ftctx,
-		program: program,
-		last:    time.Now(),
-		inc:     inc,
-	}
-	sq.buf = glctx.CreateBuffer()
-	glctx.BindBuffer(gl.ARRAY_BUFFER, sq.buf)
-	glctx.BufferData(gl.ARRAY_BUFFER, triangleData, gl.STATIC_DRAW)
+	ftctx.SetDPI(312)
+	ftctx.SetSrc(image.NewUniform(colornames.Map["black"]))
+	ftctx.SetDst(img1.RGBA)
+	ftctx.SetClip(img1.RGBA.Bounds())
+	ftctx.SetHinting(ifont.HintingFull)
 
-	sq.position = glctx.GetAttribLocation(program, "position")
-	sq.color = glctx.GetUniformLocation(program, "color")
-	sq.offset = glctx.GetUniformLocation(program, "offset")
+	sq := &Square{
+		images: images,
+		img1:   img1,
+		ftctx:  ftctx,
+	}
 	return sq, nil
 }
 
@@ -94,86 +58,30 @@ func (sq *Square) Close(glctx gl.Context) {
 	if sq == nil {
 		return
 	}
-	glctx.DeleteProgram(sq.program)
-	glctx.DeleteBuffer(sq.buf)
 
 	sq.img1.Release()
 	sq.images.Release()
 }
 
-func (sq *Square) Draw(glctx gl.Context, sz size.Event) {
+func (sq *Square) Draw(glctx gl.Context, sz size.Event, major, minor string) {
 	if sq == nil {
 		return
 	}
-	const (
-		coordsPerVertex = 3
-		vertexCount     = 6
-	)
-	glctx.UseProgram(sq.program)
-
-	now := time.Now()
-	var inc = float32(0.01) * float32(now.Sub(sq.last).Seconds()*1000) / 15
-	sq.last = now
-	if sq.down {
-		sq.green -= inc
-	} else {
-		sq.green += inc
-	}
-	var (
-		limHigh float32 = 0.9
-		limLow  float32 = 0.2
-	)
-	if (!sq.down && sq.green >= limHigh) || (sq.down && sq.green <= limLow) {
-		sq.down = !sq.down
-	}
-	if sq.green < limLow {
-		sq.green = limLow
-	}
-	if sq.green > limHigh {
-		sq.green = limHigh
-	}
-	glctx.Uniform4f(sq.color, 0, sq.green, 0, 1)
-
-	glctx.Uniform2f(sq.offset, sq.x/float32(sz.WidthPx), sq.y/float32(sz.HeightPx))
-
-	glctx.BindBuffer(gl.ARRAY_BUFFER, sq.buf)
-	glctx.EnableVertexAttribArray(sq.position)
-	glctx.VertexAttribPointer(sq.position, coordsPerVertex, gl.FLOAT, false, 0, 0)
-	glctx.DrawArrays(gl.TRIANGLES, 0, vertexCount)
-	glctx.DisableVertexAttribArray(sq.position)
-
 	draw.Copy(sq.img1.RGBA, image.ZP, image.NewUniform(colornames.Map["deepskyblue"]), sq.img1.RGBA.Bounds(), draw.Src, nil)
 
 	ftctx := sq.ftctx
+
 	ftctx.SetFontSize(12)
-	ftctx.SetDPI(312)
-	ftctx.SetSrc(image.NewUniform(colornames.Map["black"]))
-	ftctx.SetDst(sq.img1.RGBA)
-	ftctx.SetClip(sq.img1.RGBA.Bounds())
-	ftctx.SetHinting(ifont.HintingFull)
-	ftctx.DrawString("Hello Mobile", fixed.Point26_6{X: 100, Y: 10000})
+	ftctx.DrawString(major, fixed.Point26_6{X: 100, Y: 7000})
+
+	ftctx.SetFontSize(10)
+	ftctx.DrawString(minor, fixed.Point26_6{X: 100, Y: 11000})
+
+	b := sq.img1.RGBA.Bounds()
 
 	sq.img1.Upload()
-	sq.img1.Draw(sz, geom.Point{X: 0, Y: 0}, geom.Point{X: 50, Y: 0}, geom.Point{X: 0, Y: 25}, sq.img1.RGBA.Bounds())
+	sq.img1.Draw(sz, geom.Point{X: 0, Y: 0}, geom.Point{X: geom.Pt(b.Max.X) / 4, Y: 0}, geom.Point{X: 0, Y: geom.Pt(b.Max.Y) / 4}, sq.img1.RGBA.Bounds())
 }
 func (sq *Square) SetLocation(x, y float32) {
-	sq.x, sq.y = x, y
+	// sq.x, sq.y = x, y
 }
-
-const vertexShader = `#version 100
-uniform vec2 offset;
-
-attribute vec4 position;
-void main() {
-	// offset comes in with x/y values between 0 and 1.
-	// position bounds are -1 to 1.
-	vec4 offset4 = vec4(2.0*offset.x-1.0, 1.0-2.0*offset.y, 0, 0);
-	gl_Position = position + offset4;
-}`
-
-const fragmentShader = `#version 100
-precision mediump float;
-uniform vec4 color;
-void main() {
-	gl_FragColor = color;
-}`
